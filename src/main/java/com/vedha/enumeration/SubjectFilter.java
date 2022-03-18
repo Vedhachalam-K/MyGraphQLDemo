0369a236ee91f9eb1792dad84359e30a024ac72d
vedhachalam_k <vedhachalam_vit@yahoo.co.in>
2022-03-17 21:16:02 -0400

    Addition of EdgeFilter for subject list
    Addition of EdgeFilter for subject list

---
+++ src/main/java/com/vedha/enumeration/SubjectFilter.java
@@ -0,0 +1,9 @@
+package com.vedha.enumeration;
+
+public enum SubjectFilter {
+    All,
+    English,
+    Tamil,
+    Maths,
+    Physics
+}
--- src/main/java/com/vedha/resolver/StudentResponseResolver.java
+++ src/main/java/com/vedha/resolver/StudentResponseResolver.java
@@ -3,6 +3,7 @@ package com.vedha.resolver;
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.vedha.entity.Student;
 import com.vedha.entity.Subject;
+import com.vedha.enumeration.SubjectFilter;
 import com.vedha.response.StudentResponse;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +13,18 @@ import java.util.stream.Collectors;
 @Service
 public class StudentResponseResolver implements GraphQLResolver<StudentResponse> {
 
-    public List<String> getSubjectList(StudentResponse sr) {
+    public List<String> getSubjectList(StudentResponse sr, SubjectFilter subjectFilter) {
         Student st = sr.getStudent();
-        return st.getSubjectList().stream().map(Subject::getSubjectName).collect(Collectors.toList());
+        String inputSubject = subjectFilter.name();
+
+        if(!inputSubject.equalsIgnoreCase(SubjectFilter.All.name())){
+            return st.getSubjectList().stream().filter(sb -> inputSubject.equalsIgnoreCase(sb.getSubjectName()))
+                    .map(Subject::getSubjectName)
+                    .collect(Collectors.toList());
+        }else{
+            return st.getSubjectList().stream().map(Subject::getSubjectName).collect(Collectors.toList());
+        }
+
     }
 
 }
